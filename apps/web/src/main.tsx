a7eac5a16a10a5e6185f00011f21339ce97cefde
chengshier <102338125+chengshier@users.noreply.github.com>
2026-08-13 14:27:49 +0800

    style: 加载事件工作台精细化样式

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -7,5 +7,6 @@ import './workspace.css'
 import './visual-fidelity.css'
 import './product-ux.css'
 import './screenshot-refinement.css'
+import './workbench-polish.css'
 
 createRoot(document.getElementById('root')!).render(<StrictMode><App /></StrictMode>)
