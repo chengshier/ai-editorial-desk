c5cd189df69712847d0cf8d2bb17d01e47342e17
chengshier <102338125+chengshier@users.noreply.github.com>
2026-08-13 16:21:28 +0800

    fix: 加载浏览器交互修复样式

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -8,5 +8,6 @@ import './visual-fidelity.css'
 import './product-ux.css'
 import './screenshot-refinement.css'
 import './workbench-polish.css'
+import './interaction-hotfix.css'
 
 createRoot(document.getElementById('root')!).render(<StrictMode><App /></StrictMode>)
